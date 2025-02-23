4d2ce0e3dc05ffc7e482b5f4b81d378479f6666e
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-02-23 18:29:25 +0530

    tickets update

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import qr_bar_code
-import url_launcher_macos
-import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  QrBarCodePlugin.register(with: registry.registrar(forPlugin: "QrBarCodePlugin"))
-  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
-  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
