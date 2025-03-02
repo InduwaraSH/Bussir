97f553875d27119ecc61657c2db884af82e93c22
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-03-02 10:57:46 +0530

    searchbar adding

---
+++ lib/searchbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Searchbar extends StatelessWidget {
+  const Searchbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(30)
+      ),
+      padding: const EdgeInsets.symmetric(horizontal: 25),
+    );
+  }
+}
