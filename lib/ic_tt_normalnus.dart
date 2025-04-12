8f3e7ed7e9747280f6a3200149c023db6cf731db
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-04-13 02:29:54 +0530

    firebase setup for android

--- lib/ic_tt_normalnus.dart
+++ lib/ic_tt_normalnus.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 
 class IntCity_tt_NorBus extends StatelessWidget {
   const IntCity_tt_NorBus({super.key});
