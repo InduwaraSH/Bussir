f5af9954b024dc81e2f4cf77452de9c78af7b60b
InduwaraSH <s.h.induwara.dil@gmail.com>
2025-06-01 01:32:57 +0530

    Nor_b_t_upadted_ui

--- lib/ic_tt_normalnus.dart
+++ lib/ic_tt_normalnus.dart
@@ -20,45 +20,139 @@ class _IntCity_tt_NorBusState extends State<IntCity_tt_NorBus> {
       padding: const EdgeInsets.all(10),
       margin: const EdgeInsets.all(10),
       height: 110,
-      color: Colors.amberAccent,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color.fromARGB(255, 255, 255, 255),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 7,
+            offset: const Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Stack(
         children: [
-          Flexible(
-            child: Text(
-              softWrap: true,
-              maxLines: 1,
-              Student['Start Location'],
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-            ),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Flexible(
-            child: Text(
-                //Student['End Location'],
-                "ddf",
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-                softWrap: true),
-          ),
-          const SizedBox(
-            height: 5,
-          ),
-          Flexible(
-            child: Text(
-              //"Start " + Student['Start Time'],
-              "dfdf",
-              maxLines: 1,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-                color: Colors.black,
+          // Container(
+          //     padding: const EdgeInsets.all(10),
+          //     margin: const EdgeInsets.all(10),
+          //     height: 110,
+          //     width: 40,
+          //     decoration: BoxDecoration(
+          //       borderRadius: BorderRadius.circular(10),
+          //       color: const Color.fromARGB(255, 95, 43, 43),
+          //     )),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Flexible(
+                child: Row(
+                  children: [
+                    Icon(Icons.location_on, color: Colors.red, size: 25),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Start Location   : ",
+                      style: TextStyle(
+                          fontFamily: 'sfpro',
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pinkAccent),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(Student['Start Location'],
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.pink,
+                            fontFamily: 'sfpro'),
+                        softWrap: true)
+                  ],
+                ),
               ),
-            ),
+              const SizedBox(
+                height: 5,
+              ),
+              Flexible(
+                child: Row(
+                  children: [
+                    Icon(Icons.location_city,
+                        color: Colors.lightGreen, size: 25),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "End Location     : ",
+                      style: TextStyle(
+                          fontFamily: 'sfpro',
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.lightGreen),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(Student['End Location'],
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.lightGreen[800],
+                            fontFamily: 'sfpro'),
+                        softWrap: true),
+                    SizedBox(
+                      width: 30,
+                    ),
+                    // Icon(, color: Colors.blue, size: 25),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              Flexible(
+                child: Row(
+                  children: [
+                    Icon(Icons.access_time_filled,
+                        color: Colors.blue, size: 25),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Start Time          : ",
+                      style: TextStyle(
+                          fontFamily: 'sfpro',
+                          fontSize: 19,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(Student['Start Time'],
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.blue[800],
+                            fontFamily: 'sfpro'),
+                        softWrap: true)
+                  ],
+                ),
+              ),
+            ],
           ),
+          Positioned(
+              right: 10,
+              top: 10,
+              child: Container(
+                width: 50,
+                height: 110,
+                color: Colors.blue,
+              )),
         ],
       ),
     );
@@ -68,7 +162,7 @@ class _IntCity_tt_NorBusState extends State<IntCity_tt_NorBus> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("h"),
+          title: Text("Normal Bus Time Table"),
         ),
         body: Container(
           height: double.infinity,
